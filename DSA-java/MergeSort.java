0bd5131a312236af7ae8f036bed4fab9ab0248ac
dinesh13p <159114512+dinesh13p@users.noreply.github.com>
2025-02-25 06:58:40 +0545

    Merge Sort

--- DSA-java/MergeSort.java
+++ DSA-java/MergeSort.java
@@ -1,45 +1,43 @@
 public class MergeSort {
 
-    static void sort( int [] a, int start, int mid, int end){
+    static void sort(int[] a, int start, int mid, int end) {
         int k = 0;
         int size = end - start;
         int i = start;
         int j = mid;
-        int [] temp = new int[size];
+        int[] temp = new int[size];
 
-
-        while(i<mid && j<end){
-            if(a[i] < a[j]){
-                temp[k++] = a[j++];
-            }
-            else{
+        while (i < mid && j < end) {
+            if (a[i] < a[j]) {
+                temp[k++] = a[i++];
+            } else {
                 temp[k++] = a[j++];
             }
 
-            while(i < mid){
+            while (i < mid) {
+                temp[k++] = a[i++];
+            }
+            while (j < end) {
                 temp[k++] = a[j++];
             }
-            while(j < end){
-                temp[k++] = a[j++];
-            }
-            for( i = start; i < end; i++){
+            for (i = start; i < end; i++) {
                 a[i] = temp[i];
             }
-            for( i =0; i < end; i++){
-                System.out.println(a[i]);
+            for (i = 0; i < end; i++) {
+                System.out.print("  " + a[i]);
             }
         }
 
     }
 
     public static void main(String[] args) {
-        int [] a = {2, 6, 8, 32, 1, 3, 7, 20};
-        int start = a[0];
-        int mid = a.length/2;
+        int[] a = { 2, 6, 8, 32, 1, 3, 7, 20 };
+        int start = 0;
         int end = a.length;
+        int mid = a.length / 2;
         sort(a, start, mid, end);
 
-        System.out.println("------------------------");
+        System.out.println("\n----------------------------");
     }
-    
+
 }
